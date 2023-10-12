dfb5212238fe1a2fc8f518e7dbf9d3f2acf4cff6
Bruno Leone <leonecbruno@gmail.com>
2023-10-12 01:31:08 -0300

    change multi pictures

--- source/src/app/shared/components/b2b-product-card/b2b-product-card.component.ts
+++ source/src/app/shared/components/b2b-product-card/b2b-product-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -9,9 +10,17 @@ import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 export class B2bProductCardComponent {
   currentRate = 3;
 
-  constructor(config: NgbRatingConfig) {
+  random = Math.floor(Math.random() * 1000 - 1);
+
+  images = [this.random].map((n) => `https://picsum.photos/id/${n}/900/1250`);
+
+  constructor(private config: NgbRatingConfig, private sanitizer: DomSanitizer,) {
     // customize default values of ratings used by this component tree
     config.max = 5;
     config.readonly = true;
+  }
+
+  getProductSafePicture(url: string) {
+    return this.sanitizer.bypassSecurityTrustUrl(url);
   }
 }
