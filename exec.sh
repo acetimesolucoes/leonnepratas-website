156b3ef340eccc92e8d957fb73ac4a8d5a4616a0
Bruno Leone <leonecbruno@gmail.com>
2023-10-24 20:52:47 -0300

    change container name

--- exec.sh
+++ exec.sh
@@ -1,2 +1,2 @@
-docker build -t acetimesolucoes/leonnepratas-ssr .
-docker run -p 4000:4000 -d acetimesolucoes/leonnepratas-ssr
+docker build -t acetimesolucoes/leonnepratas-client-ssr .
+docker run -p 4000:4000 -d acetimesolucoes/leonnepratas-client-ssr
