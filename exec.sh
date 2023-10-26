01b0fcb232f49d17f7545199f1478196c9f6e954
Bruno Leone <leonecbruno@gmail.com>
2023-10-26 02:08:59 -0300

    start terraform configuration

--- exec.sh
+++ exec.sh
@@ -1,2 +1,2 @@
 docker build -t acetimesolucoes/leonnepratas-client-ssr .
-docker run -p 4000:4000 -d acetimesolucoes/leonnepratas-client-ssr
+docker run --rm --name leonnepratas-client-ssr -d -p 4000:4000 acetimesolucoes/leonnepratas-client-ssr
