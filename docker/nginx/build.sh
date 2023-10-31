6249e5f900cd133f430f80db6f99d78eaf6fd123
Bruno Leone <leonecbruno@gmail.com>
2023-10-30 21:17:58 -0300

    change docker files

--- docker/nginx/build.sh
+++ docker/nginx/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Remove old images
-docker rm -f leonnepratas_client_ssr
+docker rm -f acetimesolucoes/leonnepratas_client_ssr
 
 # No cache build
-docker build --no-cache -t leonnepratas_client_ssr -f docker/nginx/Dockerfile .
+docker build --no-cache -t acetimesolucoes/leonnepratas_client_ssr -f docker/nginx/Dockerfile .
--- infra-old/exec.sh
+++ infra-old/exec.sh
@@ -1,4 +1,4 @@
-docker build -t acetimesolucoes/leonnepratas-client-ssr:latest .
-# docker run --name leonnepratas-client-ssr -d -p 80:4000 acetimesolucoes/leonnepratas-client-ssr:latest
+docker build -t leonnepratas-client-ssr:latest Dockerfile
+# docker run --name leonnepratas-client-ssr -d -p 80:4000 leonnepratas-client-ssr:latest
 
-docker run --name leonnepratas-client -d -p 80:4000 acetimesolucoes/leonnepratas-client-ssr:latest
+docker run --name leonnepratas-client -d -p 80:4000 leonnepratas-client-ssr:latest
